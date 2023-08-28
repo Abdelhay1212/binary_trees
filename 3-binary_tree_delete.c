6ebba0339d2e11f578691dfea1e960b4e648bd15
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-08-28 16:39:12 +0200

    task 4

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_delete - it deletes a binary tree
+ * @tree: binary tree to be deleted
+ * Return: void
+ */
 void binary_tree_delete(binary_tree_t *tree)
 {
     if (tree == NULL)
