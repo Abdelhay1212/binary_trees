772c5495540d61e37617256d86f2aa864ab36839
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-08-28 13:37:23 +0200

    task 1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+    binary_tree_t *node = malloc(sizeof(binary_tree_t));
+    binary_tree_t *left_node = malloc(sizeof(binary_tree_t));
+
+    if (parent == NULL)
+    {
+        free(node);
+        free(left_node);
+        return NULL;
+    }
+
+    node->n = value;
+    node->left = NULL;
+    node->right = NULL;
+
+    if (parent->left == NULL)
+    {
+        parent->left = node;
+        free(left_node);
+        return node;
+    }
+
+    left_node = parent->left;
+    parent->left = node;
+    node->left = left_node;
+
+    return node;
+}
