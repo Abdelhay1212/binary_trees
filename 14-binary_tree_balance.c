0a3664a5e1fa34369761811bd199da846a7229b0
Abdelhay1212 <110763003+Abdelhay1212@users.noreply.github.com>
2023-08-29 17:10:53 +0200

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,17 +7,17 @@
  */
 int tree_height(const binary_tree_t *tree)
 {
-    int lh, rh;
+	int lh, rh;
 
-    if (tree == NULL)
-    {
-        return 0;
-    }
+	if (tree == NULL)
+	{
+		return 0;
+	}
 
-    lh = tree_height(tree->left);
-    rh = tree_height(tree->right);
+	lh = tree_height(tree->left);
+	rh = tree_height(tree->right);
 
-    return (lh > rh ? lh : rh) + 1;
+	return (lh > rh ? lh : rh) + 1;
 }
 
 /**
@@ -27,13 +27,13 @@ int tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-    int left_height, right_height;
+	int left_height, right_height;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    left_height = tree_height(tree->left);
-    right_height = tree_height(tree->right);
+	left_height = tree_height(tree->left);
+	right_height = tree_height(tree->right);
 
-    return (left_height - right_height);
+	return (left_height - right_height);
 }
