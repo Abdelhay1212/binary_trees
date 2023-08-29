857a63c7a2e477e8798312cc33af30b74756e584
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-08-29 10:53:19 +0200

    updated

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -10,7 +10,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
     binary_tree_t *node = malloc(sizeof(binary_tree_t));
 
-    if (parent == NULL)
+    if (parent == NULL || node == NULL)
     {
         free(node);
         return NULL;
