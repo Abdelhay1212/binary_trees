79356b28244257cf378d99b52c3bf16423666043
Abdelhay1212 <110763003+Abdelhay1212@users.noreply.github.com>
2023-08-29 10:17:37 +0200

    Update 9-binary_tree_height.c

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,13 +7,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    size_t lh, rh;
+	size_t lh, rh;
 
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+		return (0);
 
-    lh = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-    rh = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+	lh = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	rh = tree->right ? 1 + binary_tree_height(tree->right) : 0;
 
-    return (lh > rh ? lh : rh);
+	return (lh > rh ? lh : rh);
 }
