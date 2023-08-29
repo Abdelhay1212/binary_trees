68bcc2a15966bfbed6aa27fc21ab2ee33bcea033
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-08-29 10:03:06 +0200

    updated

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,10 +10,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
     size_t lh, rh;
 
     if (tree == NULL)
-        return (-1);
+        return (0);
 
     lh = binary_tree_height(tree->left);
     rh = binary_tree_height(tree->right);
 
-    return (lh > rh ? lh + 1 : rh + 1);
+    return (lh > rh ? lh : rh);
 }
