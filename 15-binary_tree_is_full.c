6eb8ff171dfd46076cf3d26ffd5ca5c0cff0ae0d
Abdelhay1212 <110763003+Abdelhay1212@users.noreply.github.com>
2023-08-30 17:16:40 +0200

    Update 15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -12,5 +12,5 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	else if (tree->left == NULL && tree->right == NULL)
 		return (1);
 	else
-		return binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right);
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
 }
