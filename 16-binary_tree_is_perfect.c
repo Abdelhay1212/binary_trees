2ce63a81aca10b721d79779788f930321448ef7c
Abdelhay1212 <ettaouafabdelhay87@gmail.com>
2023-08-31 18:29:09 +0200

    updated

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -25,10 +25,11 @@ size_t tree_height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+    int a, b;
     size_t lh, rh;
 
     if (tree == NULL)
-        return 0;
+        return (0);
 
     lh = tree_height(tree->left);
     rh = tree_height(tree->right);
@@ -45,5 +46,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
     if (!tree->left && tree->right)
         return (0);
 
-    return (binary_tree_is_perfect(tree->left) && binary_tree_is_perfect(tree->right));
+    a = binary_tree_is_perfect(tree->left);
+    b = binary_tree_is_perfect(tree->right);
+
+    return (a && b);
 }
